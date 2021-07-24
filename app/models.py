6c2dbd3c9d57828e78cc11f797f8475a471c8739
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-24 10:26:55 +0300

    Create the class UserProfile

--- app/models.py
+++ app/models.py
@@ -28,3 +28,14 @@ class Neighborhood(models.Model):
     def update_occupants(self):
         self.occupants += 1
         self.save()
+
+class UserProfile(models.Model):
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'user_profile')
+    first_name = models.CharField(max_length = 50,null=True)
+    last_name = models.CharField(max_length = 50,null=True)
+    bio = models.TextField(null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE)
+    email = models.EmailField(max_length = 60)
+
+    def __str__(self):
+        return self.user.username
