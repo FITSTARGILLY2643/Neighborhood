5566d3926702801730aa4f2d62cc60bdc3de07da
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-24 16:24:09 +0300

    Add the configurations functions

--- project/urls.py
+++ project/urls.py
@@ -1,7 +1,7 @@
-"""project URL Configuration
+"""neighborhoodapp URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+    https://docs.djangoproject.com/en/2.0/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,7 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url,include
+from django_registration.backends.one_step.views import RegistrationView
+from django.contrib.auth import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'',include('neighborhood.urls')),
+    url(r'^accounts/register/',
+        RegistrationView.as_view(success_url='/accounts/login'),
+        name='django_registration_register'),
+    url(r'^accounts/', include('django_registration.backends.one_step.urls')),
+    url(r'^accounts/', include('django.contrib.auth.urls')),
+    url(r'^logout/$', views.logout, {"next_page": '/'},name='logout')
 ]
