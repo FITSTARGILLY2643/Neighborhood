1ed37ba8fadf3cdcfc4f2d13431021084b331fda
FITSTARGILLY2643 <fitstargilii1194@gmail.com>
2021-07-24 10:38:30 +0300

    Create the class Busness

--- app/models.py
+++ app/models.py
@@ -39,3 +39,12 @@ class UserProfile(models.Model):
 
     def __str__(self):
         return self.user.username
+
+
+class Business(models.Model):
+    name = models.CharField(max_length = 60)
+    user = models.ForeignKey(User,on_delete = models.CASCADE,related_name = 'business_user')
+    description = models.CharField(max_length = 150,null=True)
+    neighborhood = models.ForeignKey(Neighborhood,on_delete = models.CASCADE,related_name = 'business_neighbourhood')
+    category = models.ForeignKey('Category',on_delete = models.CASCADE,null=True)
+    email = models.EmailField(max_length = 60)
